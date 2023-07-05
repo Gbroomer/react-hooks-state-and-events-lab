91360a584296964548f28ce4df2a0f956df2257f
Grant Bruemmer <grantbruemmer@gmail.com>
2023-07-05 11:20:54 -0500

    State Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+  
+  let isInCart = inCart ? "Remove From Cart" : "Add to Cart"
+  let inCartClass = inCart ? "in-cart" : ""
+  
+  const handleInCart = () => {
+    setInCart(!inCart)
+  }
   return (
-    <li className="">
+    <li className= {inCartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick = {handleInCart}>{isInCart}</button>
     </li>
   );
 }
